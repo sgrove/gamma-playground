1bc5d8e08ec55faf44ddb767193673663e4ffb10
Sean Grove <s@bushi.do>
2015-05-28 10:01:02 -0700

    More fiddling with project.clj to get advanced compilation working

--- project.clj
+++ project.clj
@@ -9,19 +9,23 @@
 
   :test-paths ["spec/clj"]
 
-  :dependencies [;;[kovasb/gamma "0.1.1"]
-                 [org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2511" :scope "provided"]
-                 [kovasb/gamma "0.1.2-SNAPSHOT"]
-                 [kovasb/gamma-driver "0.1.0-SNAPSHOT"]
-                 [kovasb/fipp "0.5.3"]
+  :dependencies [[org.clojure/clojure "1.7.0-beta2"]
+                 [org.clojure/clojurescript "0.0-3291"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [org.clojure/google-closure-library "0.0-20150505-021ed5b3"]
+                 [org.clojure/google-closure-library-third-party "0.0-20150505-021ed5b3"]
+                 [instaparse "1.4.0"]
+                 [fipp "0.6.2"]
                  [ring "1.3.2"]
                  [ring/ring-defaults "0.1.3"]
                  [compojure "1.3.1"]
                  [enlive "1.1.5"]
                  [om "0.8.0-rc1"]
                  [environ "1.0.0"]
-                 [thi.ng/geom "0.0.783"]]
+                 [thi.ng/geom "0.0.783"  :exclusions [com.google.guava/guava
+                                                      com.google.javascript/closure-compiler
+                                                      org.clojure/clojure
+                                                      org.clojure/clojurescript]]]
 
   :plugins [[lein-cljsbuild "1.0.3"]
             [lein-environ "1.0.0"]]
@@ -30,26 +34,45 @@
 
   :uberjar-name "gampg.jar"
 
-  :cljsbuild {:builds {:app {:source-paths ["src/cljs"]
+  :main "gampg.server"
+
+  :cljsbuild {:builds {:app {:source-paths ["src/cljs" "yaks/gamma/src" "yaks/gamma-driver/src"]
                              :compiler {:output-to     "resources/public/js/app.js"
                                         :output-dir    "resources/public/js/out"
                                         :source-map    "resources/public/js/out.js.map"
                                         :preamble      ["react/react.min.js"]
                                         :optimizations :none
-                                        :pretty-print  true}}}}
+                                        :pretty-print  true}}
+                       :advanced {:source-paths ["src/cljs" "yaks/gamma/src" "yaks/gamma-driver/src"]
+                                  :compiler {:output-to     "resources/public/advanced/app.js"
+                                             :output-dir    "resources/public/advanced/out"
+                                             :source-map    "resources/public/advanced/out.js.map"
+                                             :preamble      ["react/react.min.js"]
+                                             :optimizations :advanced
+                                             :pseudo-names  false
+                                             :elide-asserts true
+                                             :pretty-print  false}}}}
 
-  :profiles {:dev {:source-paths ["src/cljs" "env/dev/clj"]
+  :profiles {:dev {:source-paths ["src/cljs" "env/dev/clj" "yaks/gamma/src" "yaks/gamma-driver/src"]
                    :test-paths ["test/clj"]
 
-                   :dependencies [[figwheel "0.2.1-SNAPSHOT"]
-                                  [figwheel-sidecar "0.2.1-SNAPSHOT"]
-                                  [com.cemerick/piggieback "0.1.3"]
-                                  [weasel "0.4.2"]]
+                   :dependencies [ ;;[figwheel "0.2.1-SNAPSHOT"]
+                                  [figwheel "0.3.3"]
+                                  ;;[figwheel-sidecar "0.2.1-SNAPSHOT"]
+                                  [figwheel-sidecar "0.3.3"]
+                                  ;;[com.cemerick/piggieback "0.1.3"]
+                                  [com.cemerick/piggieback "0.2.1"]
+                                  ;;[weasel "0.4.2"]
+                                  [weasel "0.6.0"]
+                                  [org.clojure/tools.nrepl "0.2.10"]
+                                  ]
 
                    :repl-options {:init-ns gampg.server
                                   :nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
 
-                   :plugins [[lein-figwheel "0.2.1-SNAPSHOT"]]
+                   :plugins [ ;;[lein-figwheel "0.2.1-SNAPSHOT"]
+                             [lein-figwheel "0.3.3"]
+                             ]
 
                    :figwheel {:http-server-root "public"
                               :server-port 3449
@@ -58,14 +81,25 @@
                    :env {:is-dev true}
 
                    :cljsbuild {:test-commands { "test" ["phantomjs" "env/test/js/unit-test.js" "env/test/unit-test.html"] }
-                               :builds {:app {:source-paths ["src/cljs" "env/dev/cljs"]}
-                                        :test {:source-paths ["src/cljs" "test/cljs"]
+                               :builds {:app {:source-paths ["src/cljs" "env/dev/cljs" "yaks/gamma/src" "yaks/gamma-driver/src"]}
+                                        :test {:source-paths ["src/cljs" "test/cljs"  "yaks/gamma/src" "yaks/gamma-driver/src"]
                                                :compiler {:output-to     "resources/public/js/app_test.js"
                                                           :output-dir    "resources/public/js/test"
                                                           :source-map    "resources/public/js/test.js.map"
                                                           :preamble      ["react/react.min.js"]
                                                           :optimizations :whitespace
-                                                          :pretty-print  false}}}}}
+                                                          :pretty-print  false}}
+                                        :prod {:id           "prod"
+                                               :source-paths ["src/cljs" "env/dev/cljs" "yaks/gamma/src" "yaks/gamma-driver/src"]
+                                               :compiler     {:asset-path    "/js/bin"
+                                                              :main          gampg.core
+                                                              :output-to     "resources/public/js/bin/main.js"
+                                                              :output-dir    "resources/public/js/bin"
+                                                              :optimizations :advanced
+                                                              ;;:pretty-print  true
+                                                              :preamble      ["react/react.min.js"]
+                                                              :externs       ["react/externs/react.js"]
+                                                              }}}}}
 
              :uberjar {:source-paths ["env/prod/clj"]
                        :hooks [leiningen.cljsbuild]
